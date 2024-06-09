d1256f79004b947eda9badedcf764095bdb3ebad
MobinAskariN <160580813+MobinAskariN@users.noreply.github.com>
2024-06-09 17:51:55 +0330

    changing Client class

--- src/main/java/espresso/youtube/Client/Client.java
+++ src/main/java/espresso/youtube/Client/Client.java
@@ -1,25 +1,24 @@
 package espresso.youtube.Client;
 
-import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.Socket;
-import java.util.Scanner;
 
 // Client Class
 public class Client {
 
     private static final String SERVER_IP = "127.0.0.1";
     private static final int PORT = 3000;
+    private Socket client;
+    private DataOutputStream out;
+
+
+    public Client() throws IOException {
+        client = new Socket(SERVER_IP, PORT);
+        out = new DataOutputStream(client.getOutputStream());
+        System.out.println("[CLIENT] " + client.getInetAddress() + " connected to server.");
+    }
 
     public static void main(String[] args) throws IOException {
-        Socket client = new Socket(SERVER_IP, PORT);
-        System.out.println("[CLIENT] " + client.getInetAddress() + " connected to server.");
-        DataOutputStream out = new DataOutputStream(client.getOutputStream());
-
-        Handle_Server_Response handleServerResponse = new Handle_Server_Response(client);
-        new Thread(handleServerResponse).start();
-
     }
 }
